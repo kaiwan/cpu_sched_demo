968cf4a02e6b243eae669726071ceb6df39daf69
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2022-11-23 08:59:25 +0530

    minor cleanups

--- demo_app/sched_pthrd_rtprio.c
+++ demo_app/sched_pthrd_rtprio.c
@@ -32,8 +32,8 @@ void *thrd_p2(void *msg)
 	 * };
 	 */
 	printf("  RT Thread p2 (LWP %d) here in function %s()\n"
-			"   setting sched policy to SCHED_FIFO and RT priority to %ld in 2 seconds..\n",
-				getpid(), __func__, (long)msg);
+	       "   setting sched policy to SCHED_FIFO and RT priority to %ld in 2 seconds..\n",
+	       getpid(), __func__, (long)msg);
 	sleep(2);
 
 	/* pthread_setschedparam(3) internally becomes the syscall sched_setscheduler(2)) */
@@ -66,12 +66,12 @@ void *thrd_p3(void *msg)
 
 	/* pthread_setschedparam(3) internally becomes the syscall sched_setscheduler(2))
 	 * Used strace to figure this out!
-     *  sudo taskset 02 strace -f ./sched_pthrd_rtprio 7 2>strc.txt
+	 *  sudo taskset 02 strace -f ./sched_pthrd_rtprio 7 2>strc.txt
 	 */
 	p.sched_priority = pri;
 	if (pthread_setschedparam(pthread_self(), SCHED_FIFO, &p))
 		perror("pthread_setschedparam");
-	sleep(4); /* blocking call; allow other thread(s) to run... */
+	sleep(4);		/* blocking call; allow other thread(s) to run... */
 
 	puts("  p3: working");
 	DELAY_LOOP('3', 210);
@@ -87,30 +87,32 @@ int main(int argc, char **argv)
 	int r, min, max;
 	long rt_pri = 1;
 
-	if (argc == 1)
-		fprintf(stderr, "Usage: %s realtime-priority\n",
-			argv[0]), exit(1);
+	if (argc == 1) {
+		fprintf(stderr, "Usage: %s realtime-priority\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 	min = sched_get_priority_min(SCHED_FIFO);
 	if (min == -1) {
 		perror("sched_get_priority_min failure");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	max = sched_get_priority_max(SCHED_FIFO);
 	if (max == -1) {
 		perror("sched_get_priority_max failure");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	MSG("SCHED_FIFO priority range is %d to %d\n", min, max);
 
-	rt_pri = atoi(argv[1]); // TODO: better to use strtoul(3) for better checking/IoF ...
+	rt_pri = atoi(argv[1]);	// TODO: better to use strtoul(3) for better checking/IoF ...
 	if ((rt_pri < min) || (rt_pri > (max - 10))) {
 		fprintf(stderr,
 			"%s: Priority value passed (%ld) out of range [%d-%d].\n",
 			argv[0], rt_pri, min, (max - 10));
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
-	printf("\nNote: to create true (soft) RT threads, and have it run as expected, you need to:\n\
+	printf
+	    ("\nNote: to create true (soft) RT threads, and have it run as expected, you need to:\n\
 	1. Run this program as superuser -OR- have the capability CAP_SYS_NICE (better!)\n\
 	2. Ensure it runs on a single CPU core (use, f.e., taskset -c02 <prg-name>)\n");
 	printf("%s() thread (%d): now creating realtime pthread p2..\n",
@@ -119,8 +121,10 @@ int main(int argc, char **argv)
 			   NULL,	// thread attributes (use default)
 			   thrd_p2,	// function to execute
 			   (void *)rt_pri);	// argument to function
-	if (r)
-		perror("pthread creation"), exit(1);
+	if (r) {
+		perror("pthread creation");
+		exit(EXIT_FAILURE);
+	}
 
 	printf("%s() thread (%d): now creating realtime pthread p3..\n",
 	       __func__, getpid());
@@ -128,9 +132,10 @@ int main(int argc, char **argv)
 			   NULL,	// thread attributes (use default)
 			   thrd_p3,	// function to execute
 			   (void *)rt_pri);	// argument to function
-	if (r)
-		perror("pthread creation"), exit(1);
-
+	if (r) {
+		perror("pthread creation");
+		exit(EXIT_FAILURE);
+	}
 	DELAY_LOOP('m', 400);
 	pthread_exit(NULL);
 }
